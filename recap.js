1822abfbbced3bb9bdfde5a4ab6d6f31aeb313b2
shamim-1995 <sashamim987@gmail.com>
2023-09-20 00:30:07 +0600

    added recap section

---
+++ recap.js
@@ -0,0 +1,38 @@
+/*
+1. var let const 
+2. default parameter
+3. template string.
+4.arrow function.
+5. destructuring and spread operator.
+6.Object.keys, Object.values, Object.entries
+7.for of used in array and string.
+8.for in loop used in object.
+*/
+
+
+// template string.
+const a = 56;
+const number = [12, 23];
+const person = {
+    name: 'shamim sikdar'
+
+}
+
+const message = `Hi, ${person.name} has a: ${a} access to ${numbers [2]}`
+
+
+// default parameter.
+if(true){
+
+
+}
+
+// arrow function.
+const square = x => x * x;
+const sum = (a, b) => a + b;
+
+
+// destructuring and spread operator.
+const {age, z, ...others} = {x: 2, y: 5, z: 12, name:'object', age:22}
+
+const [first, second, ...remaining] = ['ram', 'sham', 'jodu', 'modhu'];
